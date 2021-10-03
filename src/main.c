46f9423a242bfc36de204ed43326668972471c9d
roh6608 <rohan.sthill@gmail.com>
2021-10-03 20:38:02 +1100

    addition of more options for misc

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]){
 	char con;
 	char *ent;
 	char *entries[] = {"article", "book", "booklet", "inbook", "incollection", "inproceedings", "manual", "mastersthesis", "misc", "phdthesis", "misc", "phdthesis", "proceedings", "techreport", "unpublished"};
-	char fields[13][8][15] = {{"key","author","title","journal","year","note"},{"key","author","title","publisher","year","note"},{"key","title","note"},{"key","author","title","chapter","pages","publisher","year","note"},{"key","author","title","booktitle","publisher","year","note"},{"key","author","title","booktitle","year","note"},{"key","title","note"},{"key","author","title","school","year","note"},{"key","note"},{"key","author","title","school","year","note"},{"key","title","year","note"},{"key","author","title","institution","year","note"},{"key","author","title","note","note"}};
-	int fieldSize[13] = {6,6,3,8,7,6,3,6,2,6,4,6,5};
+	char fields[13][8][15] = {{"key","author","title","journal","year","note"},{"key","author","title","publisher","year","note"},{"key","title","note"},{"key","author","title","chapter","pages","publisher","year","note"},{"key","author","title","booktitle","publisher","year","note"},{"key","author","title","booktitle","year","note"},{"key","title","note"},{"key","author","title","school","year","note"},{"key","author","title","howpublished","note"},{"key","author","title","school","year","note"},{"key","title","year","note"},{"key","author","title","institution","year","note"},{"key","author","title","note","note"}};
+	int fieldSize[13] = {6,6,3,8,7,6,3,6,5,6,4,6,5};
 	char **enter;
 	FILE *file;
 	// allocating memory
