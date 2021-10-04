942fd5befac422455e83c96f4bb8da044621bc26
roh6608 <rohan.sthill@gmail.com>
2021-10-04 11:27:42 +1100

    streamlining writing, reducing sueprfluos malloc calls that are not needed

--- src/main.c
+++ src/main.c
@@ -9,12 +9,12 @@ int main(int argc, char *argv[]){
 	char *entries[] = {"article", "book", "booklet", "inbook", "incollection", "inproceedings", "manual", "mastersthesis", "misc", "phdthesis","proceedings", "techreport", "unpublished"};
 	char fields[13][8][15] = {{"key","author","title","journal","year","note"},{"key","author","title","publisher","year","note"},{"key","title","note"},{"key","author","title","chapter","pages","publisher","year","note"},{"key","author","title","booktitle","publisher","year","note"},{"key","author","title","booktitle","year","note"},{"key","title","note"},{"key","author","title","school","year","note"},{"key","author","title","howpublished","note"},{"key","author","title","school","year","note"},{"key","title","year","note"},{"key","author","title","institution","year","note"},{"key","author","title","note","note"}};
 	int fieldSize[13] = {6,6,3,8,7,6,3,6,5,6,4,6,5};
-	char **enter;
+	char *enter;
 	FILE *file;
 
 	// allocating memory
 	ent = malloc(15*sizeof(char));
-	enter = malloc(sizeof(char)*8);
+	enter = malloc(sizeof(char)*200);
 
 
 	// opening file
@@ -38,31 +38,25 @@ int main(int argc, char *argv[]){
 			if(strcmp(ent,entries[i]) == 0){
 			len = fieldSize[i];
 
-			enter = realloc(enter, sizeof(char*)*len);
-        
 			fprintf(file,"@%s{",ent);
 
 				for(j=0;j<len;j++){
-					enter[j] = malloc(sizeof(char)*200);
 
 					printf("%s: ",fields[i][j]);
 
-					fgets(enter[j],sizeof(char)*200, stdin);
-					if((strlen(enter[j]) > 0) && (enter[j][strlen(enter[j])-1] == '\n')){
-						enter[j][strlen(enter[j])-1]='\0';
+					fgets(enter,sizeof(char)*200, stdin);
+					if((strlen(enter) > 0) && (enter[strlen(enter)-1] == '\n')){
+						enter[strlen(enter)-1]='\0';
 					}
 
 					if(j==0){
-						fprintf(file,"%s,\n",enter[j]);
+						fprintf(file,"%s,\n",enter);
 					}
 					else if(j<len-1 && j !=0){
-						fprintf(file,"%s = {%s},\n",fields[i][j],enter[j]);
+						fprintf(file,"%s = {%s},\n",fields[i][j],enter);
 					} else if(j==len-1){
-						fprintf(file,"%s = {%s}}\n\n",fields[i][j], enter[j]);
+						fprintf(file,"%s = {%s}}\n\n",fields[i][j], enter);
 					}
-				}
-				for(k=0;k<len;k++){
-					free(enter[k]);
 				}
 			}
 		}
